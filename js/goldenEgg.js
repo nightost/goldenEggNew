de7e692d5beef2fc374282adc40e4ddf2f62ab52
nightost <nightost@yeah.net>
2014-07-04 00:54:15 +0800

    change clips rotate degree

--- js/goldenEgg.js
+++ js/goldenEgg.js
@@ -28,77 +28,77 @@ function goldenEgg(){
 			$(".clip1").css({
 				left: -38,
 				top: -14,
-				"-webkit-transform": "rotate(20deg)"
+				"-webkit-transform": "rotate(380deg)"
 			});
 			$(".clip2").css({
 				left: -26,
 				top: -20,
-				"-webkit-transform": "rotate(20deg)"
+				"-webkit-transform": "rotate(380deg)"
 			});
 			$(".clip3").css({
 				left: -18,
 				top: -10,
-				"-webkit-transform": "rotate(20deg)"
+				"-webkit-transform": "rotate(380deg)"
 			});
 			$(".clip4").css({
 				left: 12,
 				top: -38,
-				"-webkit-transform": "rotate(20deg)"
+				"-webkit-transform": "rotate(380deg)"
 			});
 			$(".clip5").css({
 				left: -13,
 				top: -22,
-				"-webkit-transform": "rotate(20deg)"
+				"-webkit-transform": "rotate(380deg)"
 			});
 			$(".clip6").css({
 				left: -1,
 				top: -17,
-				"-webkit-transform": "rotate(20deg)"
+				"-webkit-transform": "rotate(380deg)"
 			});
 			$(".clip7").css({
 				left: -3,
 				top: -28,
-				"-webkit-transform": "rotate(20deg)"
+				"-webkit-transform": "rotate(380deg)"
 			});
 			$(".clip8").css({
 				left: -21,
 				top: -35,
-				"-webkit-transform": "rotate(20deg)"
+				"-webkit-transform": "rotate(380deg)"
 			});
 			$(".clip9").css({
 				left: -14,
 				top: -20,
-				"-webkit-transform": "rotate(20deg)"
+				"-webkit-transform": "rotate(380deg)"
 			});
 			$(".clip10").css({
 				left: -53,
 				top: -14,
-				"-webkit-transform": "rotate(20deg)"
+				"-webkit-transform": "rotate(380deg)"
 			});
 			$(".clip11").css({
 				left: 18,
 				top: -20,
-				"-webkit-transform": "rotate(20deg)"
+				"-webkit-transform": "rotate(380deg)"
 			});
 			$(".clip12").css({
 				left: 45,
 				top: -14,
-				"-webkit-transform": "rotate(20deg)"
+				"-webkit-transform": "rotate(380deg)"
 			});
 			$(".clip13").css({
 				left: 32,
 				top: -33,
-				"-webkit-transform": "rotate(20deg)"
+				"-webkit-transform": "rotate(380deg)"
 			});
 			$(".clip14").css({
 				left: -53,
 				top: -14,
-				"-webkit-transform": "rotate(20deg)"
+				"-webkit-transform": "rotate(380deg)"
 			});
 			$(".clip15").css({
 				left: -45,
 				top: -28,
-				"-webkit-transform": "rotate(20deg)"
+				"-webkit-transform": "rotate(380deg)"
 			});
 			
 		},400);
