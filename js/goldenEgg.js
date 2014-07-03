9dfd84237a43bbede083f5240391babea9862999
nightost <nightost@yeah.net>
2014-07-03 17:36:14 +0800

    add small pics size text

--- js/goldenEgg.js
+++ js/goldenEgg.js
@@ -1,9 +1,18 @@
 $(function(){
-
+	var _goldenEgg=new goldenEgg();
 });
 /*
 *golden egg
 */
 function goldenEgg(){
-	
+	var _eggBox=$(".egg-box");
+	var _hammer=$(".hammer");
+	//event
+	_eggBox.bind("click",function(){
+		_hammer.css({
+			"-webkit-transform":"rotate(-20deg)",
+			"-moz-transform":"rotate(-20deg)",
+			"transform":"rotate(-20deg)",
+		});
+	});
 }
