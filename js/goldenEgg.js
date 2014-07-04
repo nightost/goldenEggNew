6d2552b527a18d3716ee5360bc554d08a31ed93b
nightost <nightost@yeah.net>
2014-07-04 13:12:14 +0800

    chang once click

--- js/goldenEgg.js
+++ js/goldenEgg.js
@@ -2,6 +2,14 @@ $(function(){
 	var _goldenEgg=new goldenEgg();
 });
 /*
+*tip message
+*/
+function showMessage(str){
+	$("#tipBox span").text(str).addClass("normal").css({
+		opacity:100
+	});
+}
+/*
 *golden egg
 */
 function goldenEgg(){
@@ -10,9 +18,27 @@ function goldenEgg(){
 	var _brokenEgg=$(".broken-egg");
 	var _Egg=$(".egg");
 	var _flashLight=$(".flash-light");
+	var _clip1=$(".clip1");
+	var _clip2=$(".clip2");
+	var _clip3=$(".clip3");
+	var _clip4=$(".clip4");
+	var _clip5=$(".clip5");
+	var _clip6=$(".clip6");
+	var _clip7=$(".clip7");
+	var _clip8=$(".clip8");
+	var _clip9=$(".clip9");
+	var _clip10=$(".clip10");
+	var _clip11=$(".clip11");
+	var _clip12=$(".clip12");
+	var _clip13=$(".clip14");
+	var _clip15=$(".clip15");
 	//event
 	_eggBox.bind("click",function(){
-		_hammer.addClass("hammerEffect");
+		_hammer.animate({"-webkit-transform":"rotate(20deg)"},{"duration": 100, "easing": "ease-in","complete":function(){
+			$(this).animate({"-webkit-transform":"rotate(-25deg)"},{"duration": 200, "easing": "ease-in-out","complete":function(){
+				$(this).animate({"-webkit-transform":"rotate(0deg)"},{"duration": 200, "easing": "ease-in-out"});
+			}});
+		}});
 		setTimeout(function(){
 			_Egg.css({
 				"height":169,
@@ -25,82 +51,170 @@ function goldenEgg(){
 				"display":"block",
 				"width":224
 			});
-			$(".clip1").css({
+			_clip1.css({
 				left: -38,
 				top: -14,
 				"-webkit-transform": "rotate(380deg)"
 			});
-			$(".clip2").css({
+			_clip2.css({
 				left: -26,
 				top: -20,
 				"-webkit-transform": "rotate(380deg)"
 			});
-			$(".clip3").css({
+			_clip3.css({
 				left: -18,
 				top: -10,
 				"-webkit-transform": "rotate(380deg)"
 			});
-			$(".clip4").css({
+			_clip4.css({
 				left: 12,
 				top: -38,
 				"-webkit-transform": "rotate(380deg)"
 			});
-			$(".clip5").css({
+			_clip5.css({
 				left: -13,
 				top: -22,
 				"-webkit-transform": "rotate(380deg)"
 			});
-			$(".clip6").css({
+			_clip6.css({
 				left: -1,
 				top: -17,
 				"-webkit-transform": "rotate(380deg)"
 			});
-			$(".clip7").css({
+			_clip7.css({
 				left: -3,
 				top: -28,
 				"-webkit-transform": "rotate(380deg)"
 			});
-			$(".clip8").css({
+			_clip8.css({
 				left: -21,
 				top: -35,
 				"-webkit-transform": "rotate(380deg)"
 			});
-			$(".clip9").css({
+			_clip9.css({
 				left: -14,
 				top: -20,
 				"-webkit-transform": "rotate(380deg)"
 			});
-			$(".clip10").css({
+			_clip10.css({
 				left: -53,
 				top: -14,
 				"-webkit-transform": "rotate(380deg)"
 			});
-			$(".clip11").css({
+			_clip11.css({
 				left: 18,
 				top: -20,
 				"-webkit-transform": "rotate(380deg)"
 			});
-			$(".clip12").css({
+			_clip12.css({
 				left: 45,
 				top: -14,
 				"-webkit-transform": "rotate(380deg)"
 			});
-			$(".clip13").css({
+			_clip13.css({
 				left: 32,
 				top: -33,
 				"-webkit-transform": "rotate(380deg)"
 			});
-			$(".clip14").css({
+			_clip14.css({
 				left: -53,
 				top: -14,
 				"-webkit-transform": "rotate(380deg)"
 			});
-			$(".clip15").css({
+			_clip15.css({
 				left: -45,
 				top: -28,
 				"-webkit-transform": "rotate(380deg)"
 			});
-			
+			showMessage("恭喜，您中得帅哥数枚");
 		},400);
 	});
+	this.reset=function(){
+		_Egg.css({
+			"height":110,
+			"opacity":100
+		});
+		_brokenEgg.css({
+			"display":"none"
+		});
+		_flashLight.css({
+			"display":"none",
+			"width":112
+		});
+		_clip1.css({
+			left:0,
+			top:0,
+			"-webkit-transform": "rotate(0deg)"
+		});
+		_clip2.css({
+			left:0,
+			top:0,
+			"-webkit-transform": "rotate(0deg)"
+		});
+		_clip3.css({
+			left:0,
+			top:0,
+			"-webkit-transform": "rotate(0deg)"
+		});
+		_clip4.css({
+			left:0,
+			top:0,
+			"-webkit-transform": "rotate(0deg)"
+		});
+		_clip5.css({
+			left:0,
+			top:0,
+			"-webkit-transform": "rotate(0deg)"
+		});
+		_clip6.css({
+			left:0,
+			top:0,
+			"-webkit-transform": "rotate(0deg)"
+		});
+		_clip7.css({
+			left:0,
+			top:0,
+			"-webkit-transform": "rotate(0deg)"
+		});
+		_clip8.css({
+			left:0,
+			top:0,
+			"-webkit-transform": "rotate(0deg)"
+		});
+		_clip9.css({
+			left:0,
+			top:0,
+			"-webkit-transform": "rotate(0deg)"
+		});
+		_clip10.css({
+			left:0,
+			top:0,
+			"-webkit-transform": "rotate(0deg)"
+		});
+		_clip11.css({
+			left:0,
+			top:0,
+			"-webkit-transform": "rotate(0deg)"
+		});
+		_clip12.css({
+			left:0,
+			top:0,
+			"-webkit-transform": "rotate(0deg)"
+		});
+		_clip13.css({
+			left:0,
+			top:0,
+			"-webkit-transform": "rotate(0deg)"
+		});
+		_clip14.css({
+			left:0,
+			top:0,
+			"-webkit-transform": "rotate(0deg)"
+		});
+		_clip15.css({
+			left:0,
+			top:0,
+			"-webkit-transform": "rotate(0deg)"
+		});
+	}
 }
